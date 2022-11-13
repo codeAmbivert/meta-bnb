66f85218b9999eeb6f921b54a4740bba2a22e85c
codingAmbivert <titilopechisom20@gmail.com>
2022-11-13 22:23:58 +0100

    more mediaQuery update

--- src/Footer.js
+++ src/Footer.js
@@ -24,26 +24,28 @@ function Footer() {
               </Link>
             </div>
           </div>
-          <div className="footer__community" id="community">
-            <div className="_header">Community</div>
-            <p>NFT</p>
-            <p>Tokens</p>
-            <p>Landlords</p>
-            <p>Discord</p>
-          </div>
-          <div className="footer__place" id="place">
-            <div className="_header">Places</div>
-            <p>Castle</p>
-            <p>Farms</p>
-            <p>Beach</p>
-            <p>Learn more</p>
-          </div>
-          <div className="footer__about" id="about">
-            <div className="_header">About us</div>
-            <p>Road maps</p>
-            <p>Creators</p>
-            <p>Career</p>
-            <p>Contacts</p>
+          <div>
+            <div className="footer__community" id="community">
+              <div className="_header">Community</div>
+              <p>NFT</p>
+              <p>Tokens</p>
+              <p>Landlords</p>
+              <p>Discord</p>
+            </div>
+            <div className="footer__place" id="place">
+              <div className="_header">Places</div>
+              <p>Castle</p>
+              <p>Farms</p>
+              <p>Beach</p>
+              <p>Learn more</p>
+            </div>
+            <div className="footer__about" id="about">
+              <div className="_header">About us</div>
+              <p>Road maps</p>
+              <p>Creators</p>
+              <p>Career</p>
+              <p>Contacts</p>
+            </div>
           </div>
         </div>
         <div className="copyright">&copy; 2022 Metabnb</div>
